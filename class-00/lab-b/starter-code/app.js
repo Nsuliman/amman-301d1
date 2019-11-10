5a6df68583821d985c6e1420222c2fc1472cd1d4
Nsuliman <nsuliman88@gmail.com>
2019-11-10 13:15:54 +0200

    Step 10 fun 4

--- class-00/lab-b/starter-code/app.js
+++ class-00/lab-b/starter-code/app.js
@@ -159,12 +159,19 @@ let sumAndProduct = (a, b) => {
 console.log(sumAndProduct(3, 9));
 
 
-let message = function(name) {
-  return `Hello, ${name}!`;
-};
+/*********** Arrow Function  message  **************************/  
 
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(message('Allie'));
+// let message = function(name) {
+//   return `Hello, ${name}!`;
+// };
+
+let message = name => `Hello, ${name}!`;
+
+
+// Done: Uncomment the following line of code to see the output in the browser console
+console.log(message('Allie'));
+
+
 
 
 let Student = function(name, age, hometown) {
