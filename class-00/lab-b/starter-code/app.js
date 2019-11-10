9cfef0d1659a5f93c3a47a7087e029af3c5344b9
Nsuliman <nsuliman88@gmail.com>
2019-11-10 23:02:21 +0200

    lab b done

--- class-00/lab-b/starter-code/app.js
+++ class-00/lab-b/starter-code/app.js
@@ -221,17 +221,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// indicate to aparticular object 
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// use the value of this in the scope we invoked 
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// So arrow functions are unusable for object methods!
